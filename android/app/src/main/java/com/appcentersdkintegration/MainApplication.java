1ff8e615c9952542320a4ffc8898746cbab60276
Shubham Zinjad <shubham.zinjad@microtelecom.com>
2020-04-02 18:54:29 +0530

    main.java  cchanged

--- android/app/src/main/java/com/appcentersdkintegration/MainApplication.java
+++ android/app/src/main/java/com/appcentersdkintegration/MainApplication.java
@@ -10,7 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.microsoft.codepush.react.CodePush;
+
 
 
 public class MainApplication extends Application implements ReactApplication {
@@ -20,8 +20,7 @@ public class MainApplication extends Application implements ReactApplication {
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
 
-            
-            
+          
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
