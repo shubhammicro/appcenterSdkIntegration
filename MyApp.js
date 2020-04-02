36d21b798f1d3d9ec50cfee49a575b70df2f7121
Shubham Zinjad <shubham.zinjad@microtelecom.com>
2020-04-02 16:25:39 +0530

    edit button

---
+++ MyApp.js
@@ -0,0 +1,39 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+let codePushOptions = { checkFrequency: codePush.CheckFrequency.MANUAL };
+
+class MyApp extends Component {
+    onButtonPress() {
+        codePush.sync({
+            updateDialog: true,
+            installMode: codePush.InstallMode.IMMEDIATE
+        });
+    }
+
+    render() {
+        <View>
+            <TouchableOpacity onPress={this.onButtonPress}>
+                <Text>Check for updates</Text>
+            </TouchableOpacity>
+        </View>
+    }
+}
+
+MyApp = codePush(codePushOptions)(MyApp);
